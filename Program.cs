5553c532ac6beffe26a56a196c0752aa7f07b0b9
behlul.sahin <behlul.sahin@ANK5-YZLMORT-02>
2022-08-08 12:22:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Diziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Behlül";
+            ogrenciler[1] = "Acar";
+            ogrenciler[2] = "Flash";
+
+            string[] ogrenciler1 = new string[3]
+            {
+                "Beho",
+                "Aho",
+                "Faho",
+            };
+
+            int[] sayilar = new int[3]
+            {
+                1,
+                2,
+                3,
+
+            };
+
+            int[] sayilar1 = new[]
+            {
+                1,
+            };
+
+            char[] adArray = // New kullanmadan da yapılıyor.
+            {
+                'B',
+                'E',
+                'H',
+                'L',
+                'Ü',
+                'L'
+            };
+
+            foreach (int intItem in sayilar)
+            {
+                Console.WriteLine(intItem);
+            }
+        }
+    }
+}
